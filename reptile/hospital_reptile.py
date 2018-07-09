e26aa438aadf0e2b65c981ac8ea31467066dd73f
ri <jrj279@163.com>
2018-07-09 14:50:07 +0800

    修改页面划分方法

--- reptile/hospital_reptile.py
+++ reptile/hospital_reptile.py
@@ -14,8 +14,34 @@ def time_delayed(delayed=3, random_l=2):
     time.sleep(delayed + random.uniform(0, random_l))
 
 
+def get_html(url):
+    proxy_list = [
+        '220.191.103.69:808',
+        '218.106.98.166:53281'
+    ]
+    proxy = {'http': proxy_list[1]}
+    proxy_support = request.ProxyHandler(proxy)
+    opener = request.build_opener(proxy_support)
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0')]
+    request.install_opener(opener)
+    html = 0
+    while html == 0:
+        try:
+            time_delayed(1)
+            response = request.urlopen(url)
+            html = response.read().decode("utf-8")
+        except Exception as e:
+            if e.code == 404:
+                print('->访问 %s 失败' % url)
+                break
+            print('->出错 重试 %s' % e)
+    # print(html)
+    return html
+
+
 def get_pq_with_url(url, use_agent=0, delayed=3):
     p = 0
+    retry = 5
     while p == 0:
         try:
             if use_agent == 0:
@@ -24,8 +50,12 @@ def get_pq_with_url(url, use_agent=0, delayed=3):
             elif use_agent == 1:
                 p = pq(get_html(url))
         except Exception as e:
-            print('%s' % e)
-            print('出错重试')
+            if retry > 0:
+                print('->出错重试 %s' % e)
+                retry -= 1
+            else:
+                print('->访问 %s 失败' % url)
+                break
     return p
 
 
@@ -46,7 +76,8 @@ def fuc_time(url, use_agent, delayed, time_out=8):
     return 0
 
 
-def operat_mysql(info=0, operation='insert'):
+# 数据库操作
+def operate_mysql(info=0, operation='insert'):
     # 打开数据库连接
     db = pymysql.connect(host='192.168.1.203', port=3306, user='root',
                          passwd='Zkhc,.2017', db='znkf_new', charset='utf8')
@@ -60,12 +91,12 @@ def operat_mysql(info=0, operation='insert'):
                               VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)',
                                             [(h_i.province, h_i.city, h_i.area, h_i.name, h_i.address, h_i.grade,
                                               h_i.phone, h_i.email, h_i.operation) for h_i in info])
-            print("完成数 : %s " % effect_row)
+            print("  完成数 : %s " % effect_row)
             print('-----------------------------------------')
             db.commit()
         except Exception as e:
             print('%s' % e)
-            print('出错 回滚')
+            print('->出错 回滚')
             print('-----------------------------------------')
             db.rollback()
         finally:
@@ -85,68 +116,14 @@ def operat_mysql(info=0, operation='insert'):
             db.close()
 
 
-# 城市页面处理
-def solve_city(province, city, base_url, test_print=0, use_agent=0, delayed=3):
-    revoke_field = ['医院网站', '医院地址', '联系电话', '医院等级', '经营方式', '电子邮箱', '重点科室', '传真号码']
-    p = fuc_time(base_url, use_agent, delayed)
-    if p == 0:
-        print('获取%s失败' % city)
-        return 0
-    print('获取%s成功' % city)
-    p_t = 0
-    area_url = {}
-    if province[-1:] == '省' or province[-3:] == '自治区':
-        for p_i in p('ul').eq(0).children().items():
-            link = p_i.children().attr('href')
-            area_url[p_i.text()] = 'http://www.a-hospital.com' + link
-    elif province[-1:] == '市':
-        for h_i in p('p').items():
-            if p_t == 0:
-                p_t = 1
-                continue
-            for j in h_i.children().items():
-                area_url[j.text()] = 'http://www.a-hospital.com' + j.attr('href')
-            break
-    hospital_info = []
-    for a in area_url:
-        d = fuc_time(area_url[a], use_agent, delayed)
-        if d == 0:
-            print('获取%s失败' % a)
-            continue
-        print('获取%s成功' % a)
-        t = d('ul').items()
-        for h_i in t:  # ->ul
-            for j in h_i.children().items():  # ->li
-                hospital = 0
-                for k in j.children().items():  # ->b,ul
-                    if hospital == 0:
-                        if len(k('b')) == 0:
-                            break
-                        elif k.text() in revoke_field:
-                            break
-                        else:
-                            hospital = Hospital(k.text(), province, city, a)
-                    else:
-                        if hospital != 0:  # ->b
-                            for x in k.children().items():  # ->li
-                                hospital.insert_info(x.text())
-                            hospital_info.append(hospital)
-                            hospital = 0
-
-    if test_print == 1:
-        # 打印
-        print('-----------------------------------------')
-        print('获取%s %s %d家医院成功' % (province, city, len(hospital_info)))
-    return hospital_info
-
-
+# 省份页面处理
 def solve_province(url, use_agent=0, delayed=3):
     base_prefix = 'http://www.a-hospital.com'
     country = fuc_time(url, use_agent, delayed)
     if country == 0:
-        print('获取全国列表失败')
+        print('->获取全国列表失败')
         return 0
-    print('获取全国列表成功')
+    print('  获取全国列表成功')
     city_dict = {}
     province_dict = {}
     area_dict = {}
@@ -190,52 +167,97 @@ def solve_province(url, use_agent=0, delayed=3):
     return area_dict
 
 
-def get_html(url):
-    proxy_list = [
-        '220.191.103.69:808',
-        '218.106.98.166:53281'
-    ]
-    proxy = {'http': proxy_list[1]}
-    proxy_support = request.ProxyHandler(proxy)
-    opener = request.build_opener(proxy_support)
-    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0')]
-    request.install_opener(opener)
-    html = 0
-    while html == 0:
-        try:
-            time_delayed(1)
-            response = request.urlopen(url)
-            html = response.read().decode("utf-8")
-        except Exception as e:
-            print('%s' % e)
-            print('出错 重试')
-    # print(html)
-    return html
+# 城市页面处理
+def solve_city(province, city, base_url, test_print=0, use_agent=0, delayed=3):
+    p = fuc_time(base_url, use_agent, delayed)
+    if p == 0:
+        print('->获取%s失败' % city)
+        return 0
+    print('  获取%s成功' % city)
+    p_t = 0
+    area_url = {}
+    if province[-1:] == '省' or province[-3:] == '自治区':
+        for p_i in p('ul').eq(0).children().items():
+            link = p_i.children().attr('href')
+            area_url[p_i.text()] = 'http://www.a-hospital.com' + link
+    elif province[-1:] == '市':
+        for h_i in p('p').items():
+            if p_t == 0:
+                p_t = 1
+                continue
+            for j in h_i.children().items():
+                area_url[j.text()] = 'http://www.a-hospital.com' + j.attr('href')
+            break
+    hospital_info = []
+    for a in area_url:
+        area_h = solve_area(area_url[a], province, city, a, use_agent, delayed)
+        if len(area_h) == 0:
+            print('->解析%s返回数为0' % a)
+            continue
+        hospital_info += area_h
+    if test_print == 1:
+        # 打印
+        print('-----------------------------------------')
+        print('  获取%s %s %d家医院成功' % (province, city, len(hospital_info)))
+    return hospital_info
+
+
+# 区域页面处理
+def solve_area(url, province, city, area, use_agent=0, delayed=3):
+    revoke_field = ['医院网站', '医院地址', '联系电话', '医院等级', '经营方式', '电子邮箱', '重点科室', '传真号码']
+    d = fuc_time(url, use_agent, delayed)
+    if d == 0:
+        print('->获取%s失败' % area)
+        return 0
+    print('  获取%s成功' % area)
+    t = d('ul').items()
+    area_hospital_list = []
+    for h_i in t:  # ->ul
+        for j in h_i.children().items():  # ->li
+            hospital = 0
+            for k in j.children().items():  # ->b,ul
+                if hospital == 0:
+                    if len(k('b')) == 0:
+                        break
+                    elif k.text() in revoke_field:
+                        break
+                    else:
+                        hospital = Hospital(k.text(), province, city, area)
+                else:
+                    if hospital != 0:  # ->b
+                        for x in k.children().items():  # ->li
+                            hospital.insert_info(x.text())
+                        area_hospital_list.append(hospital)
+                        hospital = 0
+    return area_hospital_list
 
 
 def main(url):
+    use_agent = 1
     print("Start : %s" % time.ctime())
-    city_info_dict = solve_province(url)
+    city_info_dict = solve_province(url, use_agent=use_agent)
     if city_info_dict == 0:
         exit(1)
     # 查询数据库中已包含的城市，作为剔除列表
-    c_list = operat_mysql(operation='select')
+    c_list = operate_mysql(operation='select')
     for i in city_info_dict:
-        if i in c_list:
-            continue
         city_info = city_info_dict[i]
-        print('%s  %s  %s' % (city_info[0], i, city_info[1]))
-        hospital_info_list = solve_city(city_info[0], i, city_info[1], 1)
+        print('%s  %s' % (city_info[0], i))
+        if i in c_list:
+            print('->已存入数据库')
+            continue
+        hospital_info_list = solve_city(city_info[0], i, city_info[1], 1, use_agent=use_agent)
         if hospital_info_list != 0:
-            operat_mysql(hospital_info_list)
+            operate_mysql(hospital_info_list, operation='insert')
     print("End : %s" % time.ctime())
 
 
 if __name__ == '__main__':
     country_url = 'http://www.a-hospital.com/w/%E5%85%A8%E5%9B%BD%E5%8C%BB%E9%99%A2%E5%88%97%E8%A1%A8'
-    # h_info = solve_city('上海市', '上海市',
-    #                            'http://www.a-hospital.com/w/%E4%B8%8A%E6%B5%B7%E5%B8%82%E5%8C%BB%E9%99%A2%E5%88%97%E8%A1%A8',
-    #                            1)
+    # main(country_url)
+    h_info = solve_city('广东省', '东莞市',
+                               'http://www.a-hospital.com/w/%E4%B8%9C%E8%8E%9E%E5%B8%82%E5%8C%BB%E9%99%A2%E5%88%97%E8%A1%A8',
+                               1)
     # if h_info != 0:
     #     insert_mysql(h_info)
-    main(country_url)
+
